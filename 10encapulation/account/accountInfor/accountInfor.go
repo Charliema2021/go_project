c41deaf718f603d383b986e91231269a6b9e1063
matianqi <mtq159951@163.com>
2021-06-08 10:04:55 +0800

    officeupdate\6.8

--- 10encapulation/account/accountInfor/accountInfor.go
+++ 10encapulation/account/accountInfor/accountInfor.go
@@ -9,18 +9,35 @@ type accountInfor struct {
 }
 
 // 新建一个结构体~~
-func NewAccount(id string, money float64, pwd string) {
+func NewAccount(id string, pwd string, money float64) accountInfor {
+	var ac accountInfor
 	if len(id) < 6 && len(id) > 10 {
 		fmt.Println("输入的用户名不符合要求~~~")
-	}
-
-}
-
-func (ac *accountInfor) SetID(id string) {
-	if len(id) < 6 && len(id) > 10 {
-
 	} else {
 		ac.accountID = id
+	}
+	if len(pwd) < 8 {
+		fmt.Println("输入的密码不符合要求~~~")
+	} else {
+		ac.pwd = pwd
+	}
+	if money < 100 {
+		fmt.Println("最少存100元开户~~~")
+
+	} else {
+		ac.money = money
+	}
+
+	return ac
+}
+
+// ac的修改方法~~~
+func (ac *accountInfor) SetID(id string) {
+	if len(id) < 6 && len(id) > 10 {
+		fmt.Println("输入的用户名不符合要求~~~")
+	} else {
+		ac.accountID = id
+		fmt.Println("ID更新完成！\n新ID为：", ac.accountID)
 	}
 
 }
--- 10encapulation/account/main.go
+++ 10encapulation/account/main.go
@@ -1,7 +1,13 @@
 package main
 
-func main() {
-	var ac1 = accountInfor.accontInfor{}
+import (
+	"fmt"
+	"go_project/10encapulation/account/accountInfor"
+)
 
+func main() {
+	ac1 := accountInfor.NewAccount("gsyh111111", "888888", 200.0) //如果其中一项不满足，就不能继续，跳出一下最好
+	// 对账号进行修改操作~~~~
 	ac1.SetID("dafdklfdklf")
+	fmt.Println(ac1)
 }
